d4a8aebf749617e08a86e0038e6a2b1d5b009784
Dave Bascom <dave@cumulocreative.com>
2015-10-10 11:21:12 -0400

    working on home page layout

--- packages/ui/views/frontpage/home_client.js
+++ packages/ui/views/frontpage/home_client.js
@@ -1,3 +1,8 @@
 Template.home.helpers({
   
+  // TODO: returns the organization's description
+  projectDescription: function () {
+    return "This is a pretty cool project. You're going to want to donate your time and money."
+  }
+
 });
