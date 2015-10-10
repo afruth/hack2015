c2f7516274b15bc87865f3196a4b45cf6ea1fdf2
Nicu <nicu.moanta@gmail.com>
2015-10-10 18:55:47 +0300

    donations collection

--- packages/collections/donations/donations-common.js
+++ packages/collections/donations/donations-common.js
@@ -1,6 +1,29 @@
 DB.Donations = new Mongo.Collection('donations');
 
 Schemas.DonationSchema = new SimpleSchema({
+	//TODO: add user!
+	amount: {
+		type: Number,
+		label: 'Amount'
+	},
+	project: {
+		type: String,
+		label: 'Project'
+	},
+	//time of donation
+	time: {
+		type: Date,
+		label: 'Time'
+	},
+	message: {
+		type: String,
+		label: 'Message',
+		optional: true
+	},
+	public: {
+		type: Boolean,
+		label: 'Public'
+	}
 });
 
 DB.Donations.attachSchema(Schemas.DonationSchema);
