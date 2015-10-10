80ab2f86f18a2aad1395213a0a89098d157db4b7
Nicu <nicu.moanta@gmail.com>
2015-10-10 17:45:15 +0300

    projects-collection

--- packages/collections/projects/projects-common.js
+++ packages/collections/projects/projects-common.js
@@ -11,6 +11,35 @@ Schemas.ProjectSchema = new SimpleSchema({
   description: {
     type: String,
     label: 'Description'
+  },
+  projType: {
+    
+  },
+  location: {
+    type: Object,
+    label: 'Location',
+    optional: true,
+    "location.$.lat": {
+      type: Number
+    },
+    "location.$.long": {
+      type: Number
+    }
+  },
+  beneficiaries: {
+    type: [BeneficiarySchema],
+    label: 'Beneficiaries'
+  },
+  tasks: {
+    
+  },
+  state: {
+    type: String,
+    label: 'State'
+  },
+  visible: {
+    type: Boolean,
+    label: 'Visibility'
   }
 });
 
