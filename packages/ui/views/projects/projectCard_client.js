1b3f6b35e44dcccc48226462219c89b773e2ff38
Johannes Fasching <fasching@scivision.at>
2015-10-11 10:44:20 +0300

    Percentage for Budget

--- packages/ui/views/projects/projectCard_client.js
+++ packages/ui/views/projects/projectCard_client.js
@@ -7,4 +7,15 @@ Template.projectCard.helpers({
   //description: function () {
   //  return description;
   //}
+  fundingCompletePercentage: function() {
+    console.log(this.neededFunding(), this.allocatedFunding());
+    console.log(100.0 * ( this.allocatedFunding() / this.neededFunding()));
+    if (this.neededFunding() <= 0) {
+      return 0;
+    }
+    else {
+      console.log(Math.round(100.0 * (this.allocatedFunding() / this.neededFunding())));
+      return Math.min(100, Math.round(100.0 * (this.allocatedFunding() / this.neededFunding())));
+    }
+  }
 });
